6e0c4ff70bd4d829dd10ec543657acb13db0b484
Ahmad Mustapha <ahmad.m.mustapha@hotmail.com>
2026-02-03 11:46:22 +0200

    Add script to setup ssl

--- setup-ssl.sh
+++ setup-ssl.sh
@@ -6,12 +6,12 @@ cp ./nginx/nginx.temp.conf ./nginx/nginx.conf
 
 # 2. Run Nginx
 echo "Starting Nginx..."
-docker-compose up -d nginx
+docker compose up -d nginx
 
 # 3. Run Certbot
 echo "Requesting SSL certificate..."
-docker-compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ \
-    --email amm90@mail.aub.edu --agree-tos --no-eff-email \
+docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ \
+    --email ahmad.m.mustapha@hotmail.com --agree-tos --no-eff-email \
     -d beautifulmind.health
 
 # 4. Copy main config to live config
@@ -20,6 +20,6 @@ cp ./nginx/nginx.main.conf ./nginx/nginx.conf
 
 # 5. Docker down
 echo "Stopping all services..."
-docker-compose down
+docker compose down
 
 echo "Done! You can now run 'docker-compose up -d' to start the full stack with SSL."
